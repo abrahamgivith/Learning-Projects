611ba869d96e0713aea07e7858ef9a430f0ac7e8
Givith Abraham <abrahamgivith@gmail.com>
2020-02-14 16:24:56 +0000

    Pylint score improved to 9.62 from 4.53 for tictactoe.py

---
+++ Problem solving_Python/readme.txt
@@ -0,0 +1,7 @@
+This folder has files numbered from 1,2 etc.
+
+These files contains functions which are essentially problem solvers
+
+The details or the problem are given in the docstring
+
+The function is an attempt in solving the problem
--- Tic tac Toe_Python/tictactoe.py
+++ Tic tac Toe_Python/tictactoe.py
@@ -9,14 +9,17 @@ Ver: 1.1
 '''
 
 class Tictactoe:
+    '''
+    Class implements the necessary functions for Tic Tac Toe
+    '''
 
     def __init__(self):
         self.moves = []
         self.board = [' ']*10
-        self.displayBoard()
+        self.display_board()
 
 
-    def displayBoard(self):
+    def display_board(self):
         '''
         Method to display the updated board
         '''
@@ -25,22 +28,24 @@ class Tictactoe:
         print(self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
 
 
-    def checkValidMove(self,move):
+    def check_valid_move(self, player_move):
         '''
         This function checks if the move entered by the user is a valid one or not
         :param move: string/character entered by the user
         :return: INVALID/OCCUPIED/FAIR_MOVE
         '''
 
-        if move not in range(1, 10):
-            return 'INVALID'
-        elif move in self.moves:
-            return 'OCCUPIED'
+        if player_move not in range(1, 10):
+            valid_move_ret_value = 'INVALID'
+        elif player_move in self.moves:
+            valid_move_ret_value = 'OCCUPIED'
         else:
-            return 'FAIR_MOVE'
+            valid_move_ret_value = 'FAIR_MOVE'
+
+        return valid_move_ret_value
 
 
-    def getTotalMoves(self):
+    def get_total_moves(self):
         '''
         This function returns the number total moves played in the game
         :return: total moves | int
@@ -48,12 +53,13 @@ class Tictactoe:
         return len(self.moves)
 
 
-    def checkWin(self, player):
+    def check_win(self, player):
         '''
         Check if a player has won the game
         :param player: player marker x/o
         :return: true/false
         '''
+
         player = player.upper()
         if self.board[7] == player and self.board[8] == player  and self.board[9] == player  or \
         self.board[4] == player  and self.board[5] == player  and self.board[6] == player  or \
@@ -62,41 +68,44 @@ class Tictactoe:
         self.board[8] == player  and self.board[5] == player  and self.board[2] == player  or \
         self.board[9] == player  and self.board[6] == player  and self.board[3] == player  or \
         self.board[7] == player  and self.board[5] == player  and self.board[3] == player  or \
-        self.board[1] == player  and self.board[5] == player  and self.board[9] == player :
-            return True
+        self.board[1] == player  and self.board[5] == player  and self.board[9] == player:
+            win_status = True
         else:
-            return False
+            win_status = False
 
+        return win_status
 
-    def playerMove(self,move,player):
+    def player_move(self, player_move, player):
         '''
-        This function makes the user move on board and make calls to checkWin function to see if the game is won/tied
+        This function makes the user move on board and make calls to checkWin
+        function to see if the game is won/tied
         :param move: user move | character
         :param player: marker for current player| character
         :return: 'WIN'/'TIE'/True
         '''
         # Make the move
-        self.moves.append(move)
-        self.board[move] = player.upper()
+        self.moves.append(player_move)
+        self.board[player_move] = player.upper()
 
-        if len(self.moves) <= 9 and self.checkWin(player):
+        if len(self.moves) <= 9 and self.check_win(player):
             # The player have won the game
-            self.displayBoard()
+            self.display_board()
             return 'WIN'
-        elif len(self.moves) == 9 and not self.checkWin(player):
+        if len(self.moves) == 9 and not self.check_win(player):
             # Game is tied
-            self.displayBoard()
+            self.display_board()
             return 'TIE'
-        self.displayBoard()
-        return True
+        self.display_board()
+        return ''
 
+def main():
+    '''
+    The main function. It executes the game play
+    :return:
+    '''
+    quit_game = False
 
-
-if __name__ == '__main__':
-
-    quit = False
-
-    while not quit:
+    while not quit_game:
         # Get the marker for the user
         marker = ''
         while marker == '':
@@ -104,45 +113,46 @@ if __name__ == '__main__':
                 marker = input('Enter your marker, X or O: ')
                 if marker.lower() != 'x' and marker.lower() != 'o':
                     print('Invalid marker')
-                    marker =''
-            except:
+                    marker = ''
+            except ValueError:
                 pass
 
         current_player = marker
         total_moves = 0
         game = Tictactoe()
 
-
         while total_moves < 9:
 
             # Check if it is a valid move
             check_move_validity = ''
+            move = 0
             while check_move_validity != 'FAIR_MOVE':
                 try:
                     # Get the user move
-                    move = int(input('Player {}\'s move. Enter your move (1-9): '.format(current_player.upper())))
-                    check_move_validity = game.checkValidMove(move)
+                    move = int(input('Player {}\'s move. Enter your move (1-9): \
+                         '.format(current_player.upper())))
+
+                    check_move_validity = game.check_valid_move(move)
                     if check_move_validity == 'INVALID':
                         print('Invalid option')
                     elif check_move_validity == 'OCCUPIED':
                         print('Position already occupied')
-                except:
+                except ValueError:
                     pass
 
             # Make the validated move on the board
-            game_status = game.playerMove(move, current_player)
+            game_status = game.player_move(move, current_player)
 
             if game_status == 'WIN':
                 print('\n \n \n Player {} Wins'.format(current_player.upper()))
                 break
-            elif game_status == 'TIE':
+            if game_status == 'TIE':
                 print('\n \n \n It is a tie')
                 break
-            else:
-                pass
 
-            #Check the number of moves made so far
-            total_moves = game.getTotalMoves()
+
+            # Check the number of moves made so far
+            total_moves = game.get_total_moves()
 
             # Switch the move to next player
             if current_player == 'x':
@@ -152,6 +162,10 @@ if __name__ == '__main__':
 
         # Check if the user wants to play again
         if input('Do you want to play again  y/n ? ').lower() == 'y':
-            quit = False
+            quit_game = False
         else:
-            quit = True
+            quit_game = True
+
+
+if __name__ == '__main__':
+    main()
